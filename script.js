afdedf8f5f70175f45fdd09dfeb5bfb3053babee
negisuraj080 <143009358+negisuraj080@users.noreply.github.com>
2023-12-14 13:48:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//your JS code here. If required.function secondHighest(arr) {
+function secondHighest(arr) {
     // Check if the array is empty or has only 1 element
     if (arr.length < 2) {
         return -Infinity;
@@ -7,8 +7,8 @@
     // Remove duplicate elements and sort the array in descending order
     const uniqueSortedArr = [...new Set(arr)].sort((a, b) => b - a);
 
-    // If all elements are the same, return -Infinity
-    if (uniqueSortedArr.length === 1) {
+    // If all elements are the same or there are fewer than two unique elements, return -Infinity
+    if (uniqueSortedArr.length < 2 || uniqueSortedArr[0] === uniqueSortedArr[1]) {
         return -Infinity;
     }
 
@@ -17,15 +17,34 @@
 }
 
 function Main() {
-    var n = prompt("Enter the number of elements");
+    var n = parseInt(prompt("Enter the number of elements"), 10);
+
+    // Validate that n is a valid positive integer
+    if (isNaN(n) || n <= 0 || n % 1 !== 0) {
+        alert("Please enter a valid positive integer for the number of elements.");
+        return;
+    }
+
     var arr = [];
-    
+
     // Convert input to integers and populate the array
     for (var i = 0; i < n; i++) {
-        arr[i] = parseInt(prompt("Enter element " + (i + 1)), 10);
+        var element = parseInt(prompt("Enter element " + (i + 1)), 10);
+
+        // Validate that the entered element is a valid number
+        if (isNaN(element)) {
+            alert("Please enter valid numbers for the elements.");
+            return;
+        }
+
+        arr.push(element);
     }
 
     alert(secondHighest(arr));
 }
+
+// Call the Main function
+Main();
+
 
 
